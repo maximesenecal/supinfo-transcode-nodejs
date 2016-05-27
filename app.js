d1da20c2edb6f1e3ece22c62e0dfc6a0666bb4d0
Maxime Sénécal <maximesenecal@MacBook-Pro-de-Maxime.local>
2016-05-27 16:53:47 +0200

    Commit for merge from auth-user

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/user');
 var transcoding = require('./routes/transcoding_ffmpeg');
+var encoding = require('./routes/encoding_choice');
 
 var app = express();
 
-// view engine setup
+/*
+ * Configuration des vues et du moteur Jade
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,16 +22,19 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/static', express.static( __dirname + '/public' ) );
 
-
+/*
+ * Gestion des routes
+ */
 app.use('/', routes);
-app.use('/users', users);
+app.use('/user', user);
 app.use('/transcoding', transcoding);
+app.use('/encodingchoice', encoding);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -60,6 +66,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
